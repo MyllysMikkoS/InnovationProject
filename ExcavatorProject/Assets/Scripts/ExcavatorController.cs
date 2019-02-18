539fc27331bceed6e9d3394ec319df8f9038e666
Mikko Myllys <mikko.myllys@eng.tamk.fi>
2019-02-18 22:58:30 +0200

    Added pivot points to the joints for rotating components

--- ExcavatorProject/Assets/Scripts/ExcavatorController.cs
+++ ExcavatorProject/Assets/Scripts/ExcavatorController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ExcavatorController : MonoBehaviour
 {
+    public GameObject Body;
+    public GameObject Boom;
+    public GameObject Stick;
+    public GameObject Bucket;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
