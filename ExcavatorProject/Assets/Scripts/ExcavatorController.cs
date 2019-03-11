fcd19ed0043fc4d998976f316534c6d2a614e360
Mikko Myllys <mikko.myllys@eng.tamk.fi>
2019-03-11 22:42:27 +0200

    ExcavatorController logic updated to be more logical and simple

--- ExcavatorProject/Assets/Scripts/ExcavatorController.cs
+++ ExcavatorProject/Assets/Scripts/ExcavatorController.cs
@@ -33,9 +33,11 @@ public class ExcavatorController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CurrentBoomAngle = BoomAngleZero;
+        /*CurrentBoomAngle = BoomAngleZero;
         CurrentStickAngle = StickAngleZero;
-        CurrentBucketAngle = BucketAngleZero;
+        CurrentBucketAngle = BucketAngleZero;*/
+
+        UpdateAngleData(BoomAngleZero, StickAngleZero, BucketAngleZero);
 
         SetExcavatorAngles();
         Debug.Log("ExcavatorController scene");
@@ -47,15 +49,52 @@ public class ExcavatorController : MonoBehaviour
         // for testing update angle every 50ms => 20 times per second
         if (Time.time >= nextDataUpdate)
         {
+            /* Get new joint angle values from ExcavatorData classes 
+            // For testing purposes the datas are taken from sliders, on real situation the parameters would look something like this:
+            // UpdateAngleData(ExcavatorData392.Instance.getBoom(), ExcavatorData392.Instance.getArm(), ExcavatorData392.Instance.getBucket());
+            */
+
             //if(CanListener.Instance.isConnected())
-                UpdateAngleData(50 * BoomSlider.GetComponent<Slider>().value, 110 * StickSlider.GetComponent<Slider>().value, 165 * BucketSlider.GetComponent<Slider>().value);
+                UpdateAngleData(BoomSlider.GetComponent<Slider>().value, StickSlider.GetComponent<Slider>().value, BucketSlider.GetComponent<Slider>().value);
             //else
             //    SceneManager.LoadScene(0);
             nextDataUpdate = Time.time + updateInterval;
         }
 
+        // Update Excavator model on UI every frame
         SetExcavatorAngles();
 
+        // Update distance texts
+        SetDistanceTexts();
+    }
+
+    public void SetExcavatorAngles()
+    {
+        Boom.transform.localEulerAngles = new Vector3(0, 0, 360 - CurrentBoomAngle);
+        Stick.transform.localEulerAngles = new Vector3(0, 0, 360 - CurrentStickAngle);
+        Bucket.transform.localEulerAngles = new Vector3(0, 0, 360 - CurrentBucketAngle);
+    }
+
+    /// <summary>
+    /// Call this function with joint angles as parameters and the current joint angles will be updated automatically to UI on Update().
+    /// </summary>
+    /// <param name="BoomAngle"></param>
+    /// <param name="StickAngle"></param>
+    /// <param name="BucketAngle"></param>
+    public void UpdateAngleData(float BoomAngle, float StickAngle, float BucketAngle) // For example BoomAngle = 30, StickAngle = -15 and BucketAngle = -45
+    {
+        CurrentBoomAngle = BoomAngle;
+        CurrentStickAngle = StickAngle;
+        CurrentBucketAngle = BucketAngle;
+    }
+
+    public void SetZeroPoint()
+    {
+        ZeroPoint.transform.position = TipPoint.transform.position;
+    }
+
+    public void SetDistanceTexts()
+    {
         Vector3 position = TipPoint.transform.position;
         position.y -= 1.3f;
         Height.transform.position = position;
@@ -68,28 +107,6 @@ public class ExcavatorController : MonoBehaviour
         Distance.text = DistanceValue.ToString("0.00") + " m";
     }
 
-    public void SetExcavatorAngles()
-    {
-        Boom.transform.eulerAngles = new Vector3(0, 0, CurrentBoomAngle);
-        Stick.transform.eulerAngles = new Vector3(0, 0, CurrentStickAngle);
-        Bucket.transform.eulerAngles = new Vector3(0, 0, CurrentBucketAngle);
-    }
-
-    public void UpdateAngleData(float BoomAngle, float StickAngle, float BucketAngle)
-    {
-        /*float boom = ExcavatorData392.Instance.getBoom();
-        float[] arm = ExcavatorData392.Instance.getArm();
-        float[] bucket = ExcavatorData392.Instance.getBucket();*/
-        CurrentBoomAngle = BoomAngleZero + BoomAngle;
-        CurrentStickAngle = StickAngleZero + CurrentBoomAngle + StickAngle;
-        CurrentBucketAngle = BucketAngleZero + CurrentStickAngle + BucketAngle;
-    }
-
-    public void SetZeroPoint()
-    {
-        ZeroPoint.transform.position = TipPoint.transform.position;
-    }
-
     private void OnApplicationPause(bool pause)
     {
         if (pause)
@@ -97,7 +114,6 @@ public class ExcavatorController : MonoBehaviour
             Debug.Log("PAUSE MAIN");
             CanListener.Instance.stop();
         }
-
     }
 
     private void OnApplicationQuit()
