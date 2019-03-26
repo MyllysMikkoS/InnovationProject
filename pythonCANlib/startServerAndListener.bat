3f0807d03e9be1e080eb64555242506ea23c65cd
unknown <janne.leppaharju@gmail.com>
2019-03-26 10:07:44 +0200

    test

--- pythonCANlib/startServerAndListener.bat
+++ pythonCANlib/startServerAndListener.bat
@@ -1,2 +1,4 @@
-start "webSocketServer" python webSocketServer.py
-start "canListener" python CanListener.py
+:: argument 1: ip 192.168.43.161:8765
+start "webSocketServer" python webSocketServer.py 192.168.43.161:8765
+:: argument 1: channel 0, argument 2: ip 192.168.43.161:8765
+start "canListener" python CanListener.py 0 192.168.43.161:8765
